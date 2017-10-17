2626507132f42a0ba14d4b1bf9c3824a99285ecd
Apoorv Mehrotra <apoorvmehrotra4@gmail.com>
2017-10-17 21:40:06 +0530

    updated files

---
+++ app/build/generated/source/buildConfig/debug/app/test/laitkor/mediascene/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package app.test.laitkor.mediascene;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "app.test.laitkor.mediascene";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
